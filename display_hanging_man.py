3a71d90843af77055e7d8f7cf0b1b07f1f5058a1
Frida Jacobsson <frida_jacobsson@live.se>
2022-06-02 20:24:50 +0200

    Implemented hanging man in terminal

---
+++ display_hanging_man.py
@@ -0,0 +1,59 @@
+# from: https://data-flair.training/blogs/hangman-game-python-code/
+import time
+
+
+def display_hanging_man(count, answer):
+    match count:
+        case 4:
+            print("   _____ \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(count) + " guesses remaining\n")
+        case 3:
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(count) + " guesses remaining\n")
+        case 2:
+
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(count) + " guesses remaining\n")
+
+        case 1:
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |     O \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(count) + " last guess remaining\n")
+
+        case 0:
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |     O \n"
+                  "  |    /|\ \n"
+                  "  |    / \ \n"
+                  "__|__\n")
+            print("No sorry, you are hanged! The correct answer was", answer)
