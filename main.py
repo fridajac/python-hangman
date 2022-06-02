2f075e497fe941ccb128c560e9563249ab26ee2f
Frida Jacobsson <frida_jacobsson@live.se>
2022-06-02 14:08:01 +0200

    Implemented logic in main class

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 import random
 
 # Start game by invite
-wordsLevel1 = ["Summer", "Spice", "Lilly"]
-wordsLevel2 = ["Musicplayer", "Slenderman", "Soulmate"]
-wordsLevel3 = ["Acquiesce", "Appease", "Circumspect"]
-
-
-def generate_word(level):
-    word = ""
-    match level:
-        case 1:
-            word = random.choice(wordsLevel1)
-        case 2:
-            word = random.choice(wordsLevel2)
-        case 3:
-            word = random.choice(wordsLevel3)
-    return word
+from check_level import check_level
+from generate_word import generate_word
 
 
 def start_game(name, answer):
     while True:
         guess = input('Guess a letter:')
-        if (guess in answer):
+        if guess in answer:
             print("Yes!")
 
+
 def main():
-    print("\nWelcome to Hangman game by DataFlair\n")
+    print("\nWelcome to Hangman game by Me\n")
     name = input("Enter your name: ")
-    level = input("Enter level (1-3): ")
-    print(f'Hi, {name}' + f'! Welcome to level {level})')
+    while True:
+        level = check_level(input("Enter level (1-3): "))
+        if level is not None:
+            break
+        else:
+            print("You need to enter a number between 1 -3")
+
+    print(f'Hi, {name}' + f'! Welcome to level {level}')
     word = generate_word(level)
     start_game(name, word)
+
 
 main()
