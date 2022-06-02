82071384f15e78ae2b7733ae789b86a19531b781
Frida Jacobsson <frida_jacobsson@live.se>
2022-06-02 13:01:22 +0200

    Adding generate word method and start game loop

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+import random
 
 # Start game by invite
-print("\nWelcome to Hangman game by DataFlair\n")
-name = input("Enter your name: ")
-level = input("Enter level (1-3): ")
-print(f'Hi, {name}' +f'! Welcome to level {level})')
-
-wordsLevel1 = ["Summer", "Love", "Spice", "Lilly"]
+wordsLevel1 = ["Summer", "Spice", "Lilly"]
 wordsLevel2 = ["Musicplayer", "Slenderman", "Soulmate"]
 wordsLevel3 = ["Acquiesce", "Appease", "Circumspect"]
+
+
+def generate_word(level):
+    word = ""
+    match level:
+        case 1:
+            word = random.choice(wordsLevel1)
+        case 2:
+            word = random.choice(wordsLevel2)
+        case 3:
+            word = random.choice(wordsLevel3)
+    return word
+
+
+def start_game(name, answer):
+    while True:
+        guess = input('Guess a letter:')
+        if (guess in answer):
+            print("Yes!")
+
+def main():
+    print("\nWelcome to Hangman game by DataFlair\n")
+    name = input("Enter your name: ")
+    level = input("Enter level (1-3): ")
+    print(f'Hi, {name}' + f'! Welcome to level {level})')
+    word = generate_word(level)
+    start_game(name, word)
+
+main()
