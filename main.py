a5d5b73db684ce6ea8d4cef3479c11ab945426b6
Frida Jacobsson <frida_jacobsson@live.se>
2022-06-02 11:55:14 +0200

    Created virtual env and main file

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# Start game by invite
+print("\nWelcome to Hangman game by DataFlair\n")
+name = input("Enter your name: ")
+level = input("Enter level (1-3): ")
+print(f'Hi, {name}' +f'! Welcome to level {level})')
+
+wordsLevel1 = ["Summer", "Love", "Spice", "Lilly"]
+wordsLevel2 = ["Musicplayer", "Slenderman", "Soulmate"]
+wordsLevel3 = ["Acquiesce", "Appease", "Circumspect"]
