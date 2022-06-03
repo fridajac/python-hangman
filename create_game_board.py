d92910e67b1b55fec4037e8e690237d9fc487e23
Frida Jacobsson <frida_jacobsson@live.se>
2022-06-03 10:24:24 +0200

    Fixes after pylint

--- create_game_board.py
+++ create_game_board.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
+# pylinkflake8 --disable-noqa dir/: disable=W391
 
 def create_game_board(number):
     game_board = '_' * number
